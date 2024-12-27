5163a4b84a558acde0a3174f9b6714eb77efd55e
garshin98 <gayur12@yandex.ru>
2024-12-28 02:31:42 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,13 @@
+'''created_date и issue_date
+temp_issue_date и temp_created_date
+Если мне необходимо избавиться от года при выводе в консоль даты,
+то я возьму срез от индекса начала дня и заканчивая конечным индексом месяца'''
+temp_issue_date = '10-11-2024'
+temp_created_date = '10-12-2024'
+print(temp_issue_date[:4])
+print(temp_created_date[:4])
+
+
+
+
+
